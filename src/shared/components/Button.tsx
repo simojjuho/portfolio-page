8e240c5c5f2469399e9076f72861270faf078f10
Juho Simojoki <juho.simojoki@outlook.com>
2024-04-12 22:38:53 +0300

    started testing onClick animation

--- src/shared/components/Button.tsx
+++ src/shared/components/Button.tsx
@@ -8,10 +8,18 @@ interface ButtonProps {
 const Button = ({name, onClick, isLogo=false, logo}: ButtonProps) => {
   return (
     <button
-      className="group m-2 text-xl text-gray-900 uppercase p-3 border-solid rounded-md shadow-md border-black bg-orange-400 transition duration-200 hover:bg-orange-500 hover:text-white active:shadow-none focus:bg-orange-600"
+      className="group relative m-2 text-xl uppercase p-3 border-solid rounded-md shadow-md border-black bg-orange-400 dark:bg-orange-600 transition duration-200 hover:bg-orange-700 active:shadow-none active:bg-orange-800"
       onClick={onClick}>
         {isLogo && <i className={`${logo} mr-2 transition duration-200 ease-in group-hover:scale-125`}></i>}
         {name}
+        <div
+          className="w-0 h-0 bg-orange-200 group-focus:w-full group-focus:h-full absolute opacity-5"
+        >
+          <div
+            className="w-0"
+          >
+          </div>
+        </div>
       </button>
   )
 }
